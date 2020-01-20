c16e63274dd25ec16b84bcb96c365b10eb67de04
pdxdave <webdevdave@hotmail.com>
2020-01-20 10:30:04 -0800

    german shepherd info

---
+++ src/data.js
@@ -0,0 +1,16 @@
+// https://www.akc.org/
+
+export default [
+    {
+        id: 0,
+        breed: 'German Shepherd',
+        popularity: "Ranks 2 of 195",
+        Height: '24-26 inches (male), 22-24 inches (female)',
+        Weight: '65-90 pounds (male), 50-70 pounds (female)',
+        life_expectancy: '7-10 years',
+        about: 'There are many reasons why German Shepherds stand in the front rank of canine royalty, but experts say their defining attribute is character: loyalty, courage, confidence, the ability to learn commands for many tasks, and the willingness to put their life on the line in defense of loved ones. German Shepherds will be gentle family pets and steadfast guardians, but, the breed standard says, there’s a certain aloofness that does not lend itself to immediate and indiscriminate friendships.',
+        grooming: 'The German Shepherd Dog has a medium-length, double coat consisting of a dense, harsh, and close-lying outer coat with a softer undercoat. The breed is easy to maintain, usually requiring just a quick brushing every few days or so to help remove loose hairs, but they do shed more profusely once or twice a year. During these periods, more frequent brushing will help control the amount of hair that ends up around the house and on the furniture. The German Shepherd only needs an occasional bath. It is important to trim or grind his nails every month if they are not worn down naturally, as overly long nails can cause pain and structural issues.',
+        exercise: 'As a very active and athletic breed, the German Shepherd requires lots of exercise for his physical and mental well-being. A dog who is not exercised enough will become frustrated and likely to develop undesirable behaviors. With a puppy, you can start with short daily walks, as well as play sessions in a safely fenced area. Remember to not let the dog off leash, as even the best-trained dog can become distracted and not follow every command. Participating in canine activities such as agility, herding, tracking, and dock diving provides excellent physical and mental exercise and is fun and rewarding for both dog and owner.',
+        traning: 'Early socialization and puppy training classes are vital, and continuing obedience training will help ensure that the pup will grow to be an adaptable and well-mannered adult. The German Shepherd is a highly intelligent companion and an extraordinary worker. Consistency and positive, reward-based training will yield excellent results. He is extremely bonded to his people, so he is happiest when he lives with his family. He should be raised in the household and exposed to the family’s activities. The German Shepherd Dog Club of America provides detailed training advice for owners on the club’s website.'
+    }
+]
