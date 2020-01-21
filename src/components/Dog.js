42e7278316b8eddf3638c2962916367c4e72879f
pdxdave <webdevdave@hotmail.com>
2020-01-21 15:47:23 -0800

    setting up basic files needed

---
+++ src/components/Dog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dogs = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default Dogs
---
+++ src/components/DogList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DogList = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default DogList
