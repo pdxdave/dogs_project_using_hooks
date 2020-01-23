ffadf3459d12fbad7a6a07e32e7d718ae4747958
pdxdave <webdevdave@hotmail.com>
2020-01-23 11:37:48 -0800

    added paths to homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home'
+import Dog from "./components/Dog"
+import DogList from './components/DogList'
 import {Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <div className="nav-links">
-        <Link>Home</Link>
-        <Link>The Dogs</Link>
+        <Link to="/">Home</Link>
+        <Link to="/dog-list">The Dogs</Link>
       </div>
-      <Home />
+      <Route exact path="/">
+          <Home />
+      </Route>
+      <Route path="dog-list">
+          <DogList/>
+      </Route>
+      <Route path="dog-list'/:id">
+          <Dog />
+      </Route>
+      
     </div>
   );
 }
